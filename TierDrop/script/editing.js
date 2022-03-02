1088ba8867ffacd201a852fa043d7bf7877ce644
Oltis17 <48645241+Oltis17@users.noreply.github.com>
2022-03-02 12:38:32 +0100

    Drag and Drop working in rows

--- TierDrop/script/editing.js
+++ TierDrop/script/editing.js
@@ -10,7 +10,7 @@ function DeleteR(k)
 {
     if (confirm("Delete entire row?") == true)
     {
-        k.parent().parent().remove();
+        k.parent().remove();
     }
 }
 
@@ -52,3 +52,94 @@ $(document).on('click', '.down', function ()
 {
     DownR($(this));
 });
+
+
+document.addEventListener("dragover", function(event) {
+    event.preventDefault();
+  }, false);
+
+document.addEventListener("dragenter", function( event ) {
+    // highlight potential drop target when the draggable element enters it
+    if ( event.target.className == "tier-col-m" ) {
+        event.target.style.background = "gray";
+    }
+
+}, false);
+
+document.addEventListener("dragleave", function( event ) {
+    // reset background of potential drop target when the draggable element leaves it
+    if ( event.target.className == "tier-col-m" ) {
+        event.target.style.background = "";
+    }
+
+}, false);
+
+document.addEventListener("drop", function( event ) {
+    // reset background of potential drop target when the draggable element leaves it
+    event.preventDefault();
+    if ( event.target.className == "tier-col-m" ) {
+        imageInput(event);
+        event.target.style.background = "";
+    }
+
+}, false);
+
+
+//Get DOM Elements
+let file; 
+let par;
+
+function imageInput(event) 
+{
+    
+    event.preventDefault();
+    file = event.dataTransfer.files[0];
+    console.log(event.target);
+    par =  event.target;
+    showImage(); 
+    console.log("HERE");
+};
+
+// //on drag over
+// dropArea.ondragover = (event) => {
+//   event.preventDefault();
+//   dropArea.classList.add("active");
+//   dragText.textContent = "Release to Upload File";
+// };
+
+// //on drag leave
+// dropArea.ondragleave = () => {
+//   dropArea.classList.remove("active");
+//   dragText.textContent = "Drag & Drop to Upload File";
+// };
+
+// //on drop
+// dropArea.ondrop = (event) => {
+//   event.preventDefault();
+//   file = event.dataTransfer.files[0];
+//   showImage(this);
+// };
+
+
+//change the image to droped imagege
+function showImage()
+{
+  let fileType = file.type;
+  let validFiletypes = ["image/jpeg", "image/jpg", "image/png"];
+  
+  if(validFiletypes.includes(fileType)){
+    let fileReader = new FileReader();
+    fileReader.readAsDataURL(file);
+    fileReader.onload = ()=>{
+        let fileURL = fileReader.result;
+        let image = document.createElement("img");
+        image.src = fileURL;
+        par.appendChild(image);
+        }
+    }
+  else{
+    alert("This is not an Image File!");
+    dropArea.classList.remove("active");
+    dragText.textContent = "Drag & Drop to Upload File";
+  }
+}
